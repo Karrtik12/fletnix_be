d11c28c72c5041f76053adb84d253e53c816c7f4
Karrtik12 <kartik.chat1212@gmail.com>
2024-07-22 22:45:01 +0530

    deploy

---
+++ routes/awake.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ OK: true });
+  } catch (error) {
+    console.log({ error: error.message });
+    res.status(500).send(error.message);
+  }
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const titles = require("./routes/titles");
 const auth = require("./routes/auth");
+const awake = require("./routes/awake");
 const cors = require("cors");
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/titles", titles);
 app.use("/api/auth", auth);
+app.use("/api/awake", awake);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
